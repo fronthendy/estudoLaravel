edd3c66bd1cad9214b85c2cffccf9535f7b6961e
Veronesiii <felipeveronesi1@gmail.com>
2019-08-16 18:35:48 -0300

    img, nome categoria

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,12 @@
 //     return $n1 + $n2;
 // });
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 Route::get('/produtos', 'ProdutosController@listarProdutos');
 
 Route::get('/produtos/{id}', 'ProdutosController@detalheProduto');
+
 Route::get('/produtos/categoria/{id}', 'ProdutosController@filtroCategoria');
 
 Route::get('/produtos/categoria/{id}', 'ProdutosController@filtrarCategoria');
