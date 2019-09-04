a092cc91e630c25dd6aca217016e12530cd4cf33
fronthendy <hendy.almeida@hotmail.com>
2019-09-04 19:18:04 -0300

    add middleware cors

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,12 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
+Route::get('example', array('middleware' => 'cors', 'uses' => 'ExampleController@dummy'));
 
-Route::get('produtos', 'api\ProdutosController@exibirTodos');
-Route::get('produtos/{id}', 'api\ProdutosController@detalhesProduto');
-Route::post('produtos', 'api\ProdutosController@criarProduto');
-Route::put('produtos/{id}', 'api\ProdutosController@editarProduto');
-Route::delete('produtos/{id}', 'api\ProdutosController@deletarProduto');
-Route::post('produtos/review/{id}', 'api\ProdutosController@criarReview');
+
+Route::get('produtos',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@exibirTodos'));
+Route::get('produtos/{id}',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@detalhesProduto'));
+Route::post('produtos',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@criarProduto'));
+Route::put('produtos/{id}',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@editarProduto'));
+Route::delete('produtos/{id}',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@deletarProduto'));
+Route::post('produtos/review/{id}',  array('middleware' => 'cors', 'uses' => 'api\ProdutosController@criarReview'));
