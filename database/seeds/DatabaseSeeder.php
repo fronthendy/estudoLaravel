bf94b0f04e21f19285e4f13df6ad488edc437024
fronthendy <hendy.almeida@hotmail.com>
2019-09-09 13:23:12 -0300

    fix storage

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,35 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('categoria')->delete();
-        // DB::table('categoria')->insert([
-        //     ['nome' => 'Ficção', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Tecnico', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Romance', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Mangá', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Programação', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Terror', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        // ]);
+        DB::table('categoria')->delete();
+        DB::table('categoria')->insert([
+            ['nome' => 'Ficção', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Tecnico', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Romance', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Mangá', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Programação', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Terror', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ]);
 
-        // DB::table('editora')->delete();
-        // DB::table('editora')->insert([
-        //     ['nome' => 'Moderna', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Saraiva', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Sextante', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Leya', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Nova Fronteira', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Rocco', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Arqueiro', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        // ]);
+        DB::table('editora')->delete();
+        DB::table('editora')->insert([
+            ['nome' => 'Moderna', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Saraiva', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Sextante', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Leya', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Nova Fronteira', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Rocco', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Arqueiro', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
 
-        // DB::table('autor')->delete();
-        // DB::table('autor')->insert([
-        //     ['nome' => 'Dan', 'sobrenome' => 'Brown', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Marian', 'sobrenome' => 'Keys', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Milan', 'sobrenome' => 'Kundera', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Chuck', 'sobrenome' => 'Palahniuk', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-        //     ['nome' => 'Charles', 'sobrenome' => 'Bukowski', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        // ]);
+        DB::table('autor')->delete();
+        DB::table('autor')->insert([
+            ['nome' => 'Dan', 'sobrenome' => 'Brown', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Marian', 'sobrenome' => 'Keys', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Milan', 'sobrenome' => 'Kundera', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Chuck', 'sobrenome' => 'Palahniuk', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['nome' => 'Charles', 'sobrenome' => 'Bukowski', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ]);
 
         factory(Produto::class, 10)->create();
     }
